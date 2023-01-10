0c619d0631a21114e0b7f4f934157125c84aa87e
toshi.matsu <matsut0707@gmail.com>
2023-01-10 16:14:07 +0000

    FizzBuzzメソッドを利用するプログラムを作成

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,23 @@
+def FizzBuzz(num)
+  resultStr = ""
+  # 数値が3の倍数かつ5の倍数(15の倍数)
+  if num % 15 == 0
+    resultStr = "FizzBuzz"
+  # 数値が3の倍数
+  elsif num % 3 == 0
+    resultStr = "Fizz"
+  # 数値が3の倍数
+  elsif num % 5 == 0
+    resultStr = "Buzz"
+  # 上記以外の数値
+  else
+    resultStr = num.to_s
+  end
+  resultStr
+end
+
+num_max = 100
+
+(1..num_max).each do |number|
+  puts FizzBuzz(number)
+end
